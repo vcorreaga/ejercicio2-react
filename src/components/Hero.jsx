7c795fbe0f795ae98066692ad5457745893af03c
vcorreaga <viadis085@hotmail.com>
2026-04-25 15:16:33 -0500

    agrego la parte principal del inicio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+function Hero() {
+  return (
+    <section
+      style={{
+        background: "#111",
+        color: "white",
+        textAlign: "center",
+        padding: "100px 20px",
+      }}
+    >
+      <h1 style={{ fontSize: "40px" }}>
+        ESTILO URBANO <br />
+        <span style={{ color: "#f4a300" }}>REDEFINIDO</span>
+      </h1>
+
+      <p style={{ margin: "20px 0" }}>
+        Descubre nuestra colección exclusiva de ropa urbana minimalista
+      </p>
+
+      <button
+        style={{
+          background: "#f4a300",
+          border: "none",
+          padding: "12px 20px",
+          cursor: "pointer",
+        }}
+      >
+        EXPLORAR COLECCIÓN
+      </button>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
